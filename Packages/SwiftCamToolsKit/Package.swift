dc2d0e37ae738aa971cdab857cdd5df05b765df3
Lack-Of-Name <Swebst27@igs.vic.edu.au>
2025-11-19 13:13:38 +1100

    Update packages

--- Packages/SwiftCamToolsKit/Package.swift
+++ Packages/SwiftCamToolsKit/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0"),
         .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
-        .package(url: "https://github.com/MetalPetal/MetalPetal.git", from: "2.8.0")
+        .package(url: "https://github.com/MetalPetal/MetalPetal.git", branch: "master")
     ],
     targets: [
         .target(
